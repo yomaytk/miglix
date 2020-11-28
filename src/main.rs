d080191452ca55a4a6078204b34ceb5cf932e93b
yoma <ym.atonement.oy999@gmail.com>
2020-11-28 23:50:45 +0900

    build success.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,16 @@
 
 #![no_std]
-fn main() {
+#![no_main]
+
+use core::panic::PanicInfo;
+
+/// この関数はパニック時に呼ばれる
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
 }
