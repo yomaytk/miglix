06f65e717fb66af0b8fd36ab430633a82b317e31
yoma <ym.atonement.oy999@gmail.com>
2020-11-29 01:39:11 +0900

    Hello, World./8cc -S msstest.c

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
-
 #![no_std]
 #![no_main]
 
 use core::panic::PanicInfo;
+
+static HELLO: &[u8] = b"Hello World!";
 
 /// この関数はパニック時に呼ばれる
 #[panic_handler]
@@ -12,5 +13,15 @@ fn panic(_info: &PanicInfo) -> ! {
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+
+	let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+	}
+	
     loop {}
 }
