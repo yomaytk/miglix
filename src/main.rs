a1e3d949adfb78aa3ea79a0c09a635b819d16c50
yoma <ym.atonement.oy999@gmail.com>
2020-11-30 09:24:09 +0900

    Add println! macro.

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,21 @@
 #![no_std]
 #![no_main]
 
-use core::panic::PanicInfo;
+mod vga_buffer;
 
-static HELLO: &[u8] = b"Hello World!";
+use core::panic::PanicInfo;
 
 /// この関数はパニック時に呼ばれる
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+	println!("{}", _info);
     loop {}
 }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
 
-	let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-	}
+	println!("Hello World{}", "!");
 	
     loop {}
 }
